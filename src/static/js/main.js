015fc1bcee514dc1ef7fc72177bb7f309a0ed212
Alejo <alejito23001@gmail.com>
2022-06-08 15:26:48 -0500

    NEW - UPDATE: tailwind archives and update the styles of the main page

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,9 +1,10 @@
-function generateItem() {
+worksCounter();
 
+function generateItem() {
     let parent = document.getElementById('works');
     let count = 0;
     let parentLastChild = parent.lastElementChild;
-
+    
     if (parentLastChild && parentLastChild != document.getElementById('noWorks')){
         count = parseInt(parent.lastElementChild.getAttribute('count'));
     } else {
@@ -13,7 +14,7 @@ function generateItem() {
     let div = document.createElement('div');
     div.setAttribute("class", "work-item");
     div.setAttribute("count", count + 1);
-
+    
     let lblConcept = document.createElement('label');
     lblConcept.setAttribute("for", `concept${count+1}`);
     lblConcept.innerHTML = "Concepto:";
@@ -26,11 +27,11 @@ function generateItem() {
     inputConcept.setAttribute('required', '');
     inputConcept.setAttribute('name', `concept${count+1}`)
     inputConcept.setAttribute('maxLength', '20')
-
+    
     let lblPrice = document.createElement('label');
     lblPrice.setAttribute("for", `price${count+1}`);
     lblPrice.innerHTML = "Valor:";
-
+    
     let inputPrice = document.createElement('input');
     inputPrice.setAttribute("class", "rounded-lg");
     inputPrice.setAttribute("type", "number");
@@ -38,11 +39,11 @@ function generateItem() {
     inputPrice.setAttribute("placeholder", "Ej. 1000");
     inputPrice.setAttribute('required', '');
     inputPrice.setAttribute('name', `price${count+1}`)
-
+    
     let lblQuantity = document.createElement('label');
     lblQuantity.setAttribute("for", `quantity${count+1}`);
     lblQuantity.innerHTML = "Cantidad:";
-
+    
     let inputQuantity = document.createElement('input');
     inputQuantity.setAttribute("class", "rounded-lg");
     inputQuantity.setAttribute("type", "number");
@@ -50,15 +51,16 @@ function generateItem() {
     inputQuantity.setAttribute("placeholder", "Ej. 1");
     inputQuantity.setAttribute('required', '');
     inputQuantity.setAttribute('name', `quantity${count+1}`)
-
+    
     div.appendChild(lblConcept);
     div.appendChild(inputConcept);
     div.appendChild(lblPrice);
     div.appendChild(inputPrice);
     div.appendChild(lblQuantity);
     div.appendChild(inputQuantity);
-
+    
     parent.appendChild(div);
+    worksCounter();
 }
 
 function removeItem(){
@@ -69,6 +71,7 @@ function removeItem(){
             desativateButtons(parent);
         }
     }
+    worksCounter();
 }
 
 function activateButtons(parent){
@@ -87,7 +90,17 @@ function desativateButtons(parent){
     noWorks.classList.add("text-red-500")
     parent.appendChild(noWorks);
 
-    let removeItemButton = document.getElementById('removeItemButton').style.visibility = "hidden";
-    let generateInvoiceButton = document.getElementById('generateInvoiceButton').style.visibility = "hidden";
+    document.getElementById('removeItemButton').style.visibility = "hidden";
+    document.getElementById('generateInvoiceButton').style.visibility = "hidden";
 }
 
+function worksCounter(){
+    let works = document.getElementById("works");
+    let counter = 0
+    if (works.lastChild != document.getElementById('noWorks')) {
+        counter = works.childElementCount
+    }
+    let worksLbl = document.getElementById("worksLbl");
+    worksLbl.innerText = `Trabajos (${counter}):`;
+
+}
